4430271ed4eeb986122c0d5fd2f752b320e202ad
Nishant Kumar Sahu <37589610+nishantsahu@users.noreply.github.com>
2024-02-20 16:51:31 +0000

    added signup page

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -2,9 +2,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Button.css";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
-function Button({ variant, text, prefixIcon, postfixIcon }) {
+function Button({ variant, text, prefixIcon, postfixIcon, style, onClick }) {
   return (
-    <button className={variant}>
+    <button className={variant} style={{ ...style }} onClick={onClick}>
       {prefixIcon ? (
         <FontAwesomeIcon icon={prefixIcon} style={{ marginRight: 10 }} />
       ) : null}
--- src/pages/signup/SignUpPage.jsx
+++ src/pages/signup/SignUpPage.jsx
@@ -1,7 +1,42 @@
+import Button from "../../components/Button/button";
+import AppBar from "../../components/appbar/AppBar";
+
 function SignUpPage() {
   return (
-    <div>
-      <h1>SignUpPage</h1>
+    <div className="vh10">
+      <AppBar />
+
+      <div className="container">
+        <div className="login-form">
+          <div className="form-text">
+            <span className="header-text">Looks like you're new here!</span>
+            <p className="sub-text">
+              Sign up with your mobile number to get started
+            </p>
+          </div>
+          <div className="main-form">
+            <form action="">
+              <div className="input-field">
+                <input type="text" placeholder="" required />
+                <label htmlFor="" className="floating-label">
+                  Enter Mobile Number
+                </label>
+              </div>
+              <div className="tc-text">
+                By continoung, you aggree to Flipkart's
+                <a href="/">Terms of use </a> and
+                <a href="/">Privacy Policy </a>
+              </div>
+              <Button text="Continue" variant="otp" />
+              <Button
+                text="Existing User? Log in"
+                variant="primary"
+                style={{ width: "100%", marginTop: 20 }}
+              />
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
