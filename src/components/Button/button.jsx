c9586b2778dab2c6962dc2e1eb2cbe2f52335915
Nishant Kumar Sahu <37589610+nishantsahu@users.noreply.github.com>
2024-02-20 16:53:54 +0000

    removed onclick from button component

--- src/components/Button/button.jsx
+++ src/components/Button/button.jsx
@@ -2,9 +2,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Button.css";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 
-function Button({ variant, text, prefixIcon, postfixIcon, style, onClick }) {
+function Button({ variant, text, prefixIcon, postfixIcon, style }) {
   return (
-    <button className={variant} style={{ ...style }} onClick={onClick}>
+    <button className={variant} style={{ ...style }}>
       {prefixIcon ? (
         <FontAwesomeIcon icon={prefixIcon} style={{ marginRight: 10 }} />
       ) : null}
